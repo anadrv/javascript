512a0b76ff7ad683d51ea8aa5fff26c1d0968ec1
Ana Luisa <anadrvv@gmail.com>
2025-01-06 11:20:29 -0300

    checked

--- v06 - checked property/index.js
+++ v06 - checked property/index.js
@@ -16,4 +16,14 @@ mySubmit.onclick = function() {
     } else {
         subResult.textContent = "You are NOT subscribed!";
     }
+
+    if(visaBtn.checked) {
+        paymentResult.textContent = "You are paying with Visa";
+    } else if (masterBtn.checked) {
+        paymentResult.textContent = "You are paying with Master";
+    } else if (paypalBtn.checked) {
+        paymentResult.textContent = "You are paying with PayPal";
+    } else {
+        paymentResult.textContent = "You must select a payment type";
+    }
 }
