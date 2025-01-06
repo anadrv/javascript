46e3735164ab39694087cd015e4a212943d83823
Ana Luisa <anadrvv@gmail.com>
2025-01-06 20:37:15 -0300

    ternary operator

---
+++ v07 - ternary operator/index.js
@@ -0,0 +1,15 @@
+let age = 21;
+
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+
+/**
+ *  if (age >= 18) {
+ *      message = "You're an adult"
+ * } else {
+ *      message = "You're a minor"
+ * }
+ */
+
+let time = 16;
+let greeting = time < 12 ? "Good Morning" : "Good afternoon";
