714036ac8879f32a992bd131ef8c7d66e3421d95
Ana Luisa <anadrvv@gmail.com>
2025-01-13 15:08:10 -0300

    switch case

---
+++ v08 - switch/index.js
@@ -0,0 +1,45 @@
+let day = 1;
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default: 
+        console.log(`${day} is not a day`);
+}
+
+
+let testScore = 92;
+let letterGrade;
+
+switch(true) {
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    default:
+        letterGrade = "F";
+}
