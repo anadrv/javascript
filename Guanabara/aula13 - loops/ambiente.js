3e6cfeefb1863cbd26177de7393adfd724b17d7b
Ana Luisa <anadrvv@gmail.com>
2025-04-16 10:29:17 -0300

    loops

---
+++ Guanabara/aula13 - loops/ambiente.js
@@ -0,0 +1,13 @@
+let contador = 1;
+while (contador <= 6) {
+    console.log('Tudo bem?');
+    contador++;
+}
+
+//do-while
+
+let c = 1;
+do {
+    console.log('Ok?');
+    c++;
+} while (c <= 6);
